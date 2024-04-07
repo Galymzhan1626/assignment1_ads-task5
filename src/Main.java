146818d8e8d8ef3f897dcdb667d60dcafce352f9
Galymzhan1626 <154108707+Galymzhan1626@users.noreply.github.com>
2024-04-08 01:52:28 +0500

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 import java.util.Scanner;
-
+/**
+ * The Main class contains methods to calculate Fibonacci numbers using recursion.
+ */
 public class Main {
+    /**
+     * This method calculates the nth Fibonacci number.
+     * It uses a recursive approach where the base cases are when n is 0 or 1.
+     * For n greater than 2, it returns the sum of the previous two Fibonacci numbers.
+     *
+     * @param n The position in the Fibonacci sequence.
+     * @return The nth Fibonacci number.
+     */
     public static int fibonacci(int n){
         if(n==0){
             return 0;
